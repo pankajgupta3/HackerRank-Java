29f9b6d403ff137a2300954a47f3993534e14eea
Pankaj Gupta <pankajguptakota@gmail.com>
2021-11-08 00:07:05 +0530

    Create Solution.java

---
+++ Strings/Java String Reverse/Solution.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        int i = 0;
+        int j = A.length() - 1;
+        int flag = 0;
+        while(i < j){
+            if(A.charAt(i) == A.charAt(j)){
+                i++;
+                j--;
+            }
+            else{
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 0){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
+
+
+
